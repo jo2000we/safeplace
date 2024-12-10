76420b61278e728cd8cba47bbc6cb5eff9e241af
jonathan <kontakt@wejonath.de>
2024-12-10 17:53:49 +0100

    Version Alpha 0.0.1 (Anpassung Services + HTTPS)

--- ZiehSafe/meldung/services.py
+++ ZiehSafe/meldung/services.py
@@ -1,14 +1,15 @@
+import os
 from transformers import DistilBertTokenizer, DistilBertForSequenceClassification
 from flair.models import SequenceTagger
 
+# Projekt-Basisverzeichnis
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 def get_tokenizer():
-    return DistilBertTokenizer.from_pretrained('/app/meldung/spam_model/')
-
+    return DistilBertTokenizer.from_pretrained(os.path.join(BASE_DIR, 'meldung/spam_model'))
 
 def get_model():
-    return DistilBertForSequenceClassification.from_pretrained('/app/meldung/spam_model/')
-
+    return DistilBertForSequenceClassification.from_pretrained(os.path.join(BASE_DIR, 'meldung/spam_model'))
 
 def get_tagger():
-    return SequenceTagger.load('/app/meldung/ner_model/preloaded_ner_model.pt')
+    return SequenceTagger.load(os.path.join(BASE_DIR, 'meldung/ner_model/preloaded_ner_model.pt'))
