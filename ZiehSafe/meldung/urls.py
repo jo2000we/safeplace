aa76a8d5a472e2162ada7c92f795ec46bfb39395
jonathan <kontakt@wejonath.de>
2025-01-21 16:01:33 +0100

    Version Alpha 0.0.4 (QR Code Redirect)

--- ZiehSafe/meldung/urls.py
+++ ZiehSafe/meldung/urls.py
@@ -1,10 +1,12 @@
 # meldung/urls.py
 
 from django.urls import path
+from django.shortcuts import redirect
 from . import views
 
 urlpatterns = [
     path('meldung/erstellen/', views.fall_erstellen_view, name='fall_erstellen'),
+    path('qr/', lambda request: redirect('/meldung/erstellen/', permanent=True)),
     path('meldung/erfolgreich/<str:fall_id>/', views.fall_erfolgreich_view, name='fall_erfolgreich'),
     path('', views.landing_page_view, name='landing_page'),
 ]
